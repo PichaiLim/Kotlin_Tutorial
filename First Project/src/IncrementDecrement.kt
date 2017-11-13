f7756184b4549ab5ad7044c0fbf1dd426cf99040
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-11-13 22:51:39 +0700

    commint Increment & Decrement

---
+++ First Project/src/IncrementDecrement.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var a = 10
+    var b = 8
+
+    //TODO Increment Operator
+    // a = a + 1
+    var sum = ++a + b
+
+    println("sum = ${sum}")
+
+    //TODO Decrement Operators
+    // a = a - 1
+    var dif = a ---b
+    println("dif = ${dif}")
+
+}
