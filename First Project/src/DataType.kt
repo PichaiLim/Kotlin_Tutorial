2b150c51ede91ca8c604aad2c67b200edb3f58d5
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-11-13 20:59:21 +0700

    Commit Data Type

---
+++ First Project/src/DataType.kt
@@ -0,0 +1,33 @@
+//TODO Create Main Function
+
+fun main(args: Array<String>) {
+
+    println("Data Type: ")
+
+    //TODO Data type Integer
+    //Integer
+    var a: Int = 20
+    var b: Int = -10
+    println("Value 'a' is = " + a + " and Value 'b' is = " + b + " and Sum value a - b is= " + (a - b))
+
+    // TODO Data type String
+    //String
+    var name: String = "Pichai"
+    println("args = [${name}]")
+
+    //TODO Data type Float
+    //Float
+    var sum: Float = 99.99F
+    println("sum = ${sum}")
+
+    //TODO Data type Char
+    //Char
+    var option: Char = 'a'
+    println("option = ${option}")
+
+    //TODO Data type Boolean
+    //Boolean
+    var test: Boolean = true
+    println("test = ${test}")
+
+}
