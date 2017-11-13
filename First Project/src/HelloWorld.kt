d3ab62cdff0dc72543c564965d24c04e62127e72
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-11-13 20:09:08 +0700

    Create Variable

--- First Project/src/HelloWorld.kt
+++ First Project/src/HelloWorld.kt
@@ -8,6 +8,9 @@ fun main(args: Array<String>) {
 
     println("==============")
 
+    /**
+     * Variable
+     * */
     //TODO Create Variables
     var number: Int = 20 // Fix variable type Integer Only
     // number = 1000
@@ -16,4 +19,12 @@ fun main(args: Array<String>) {
     var email: String?
     email = "example@email.com"
     println("Email :" + email)
+
+
+    var name = "Pichai"
+    var age = 31
+    var phone: Int = 123456789
+
+    println("My name is " + name + " and i am " + age + " years old and my phone number " + phone)
+
 }
