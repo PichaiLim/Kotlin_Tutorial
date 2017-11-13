f7d6f6b4bec6fe54cb6f0b043ad347c07361b245
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-11-13 21:56:13 +0700

    Commit Data Type Convert

---
+++ First Project/src/DataTypeConvert.kt
@@ -0,0 +1,24 @@
+//TODO Create Main Function
+fun main(args:Array<String>) {
+
+    //TODO Create Data Type
+    var a:Int = 20
+    var b:String = "10"
+
+    //TODO Convert Data Type
+    var c:Int
+    c=b.toInt();
+
+    println("c = ${c}")
+
+    b="200.4"
+    var d:Float
+    d=b.toFloat() //Convert Data Type
+    println("d = ${d}")
+
+    var e:Double
+    e=b.toDouble() //Convert Data Type
+    println("e = ${e}")
+
+
+}
