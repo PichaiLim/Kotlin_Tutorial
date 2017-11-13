3d4f1999dd36ca3bcd9d200229cb8c57286d80de
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2017-11-13 19:51:11 +0700

    print Your name

--- First Project/src/HelloWorld.kt
+++ First Project/src/HelloWorld.kt
@@ -1,5 +1,8 @@
 //TODO Create function main
 fun main(args:Array<String>){
     //TODO type Hello World
-    print("Hello World")
+    println("Hello World")
+
+    //TODO type Your name
+    print("My name is Mr.Pichai Limpanitivat")
 }
